be0d717b25bbec81186b2eeb0896687fe2b2d9c8
Tolik Pylypchuk <pylypchuk.tolik@gmail.com>
2025-05-05 00:08:15 +0300

    Fix detecting Mac Catalyst internally

--- SharpHook/Internal/PlatformDetector.cs
+++ SharpHook/Internal/PlatformDetector.cs
@@ -3,10 +3,14 @@ namespace SharpHook.Internal;
 [ExcludeFromCodeCoverage]
 internal static class PlatformDetector
 {
+#if !NET40_OR_GREATER && !NET6_0_OR_GREATER
+    private static readonly OSPlatform MacCatalystPlatform = OSPlatform.Create("MACCATALYST");
+#endif
+
     internal static bool IsWindows =>
 #if NET40_OR_GREATER
         Environment.OSVersion.Platform == PlatformID.Win32NT;
-#elif NET5_0_OR_GREATER
+#elif NET6_0_OR_GREATER
         OperatingSystem.IsWindows();
 #else
         RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
@@ -15,16 +19,16 @@ internal static class PlatformDetector
     internal static bool IsMacOS =>
 #if NET40_OR_GREATER
         false;
-#elif NET5_0_OR_GREATER
-        OperatingSystem.IsMacOS();
+#elif NET6_0_OR_GREATER
+        OperatingSystem.IsMacOS() || OperatingSystem.IsMacCatalyst();
 #else
-        RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
+        RuntimeInformation.IsOSPlatform(OSPlatform.OSX) || RuntimeInformation.IsOSPlatform(MacCatalystPlatform);
 #endif
 
     internal static bool IsLinux =>
 #if NET40_OR_GREATER
         Environment.OSVersion.Platform == PlatformID.Unix;
-#elif NET5_0_OR_GREATER
+#elif NET6_0_OR_GREATER
         OperatingSystem.IsLinux();
 #else
         RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
